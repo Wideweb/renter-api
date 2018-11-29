5471e1495dc92952cb2cb153f30d9ed4667432b2
Александр <alckevich@live.com>
2018-11-29 19:05:37 +0300

    auth

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-var notificationsRouter = require('./routes/notifications');
+const auth = require('./services/auth.service');
+const notificationsRouter = require('./routes/notifications');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,3 +17,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notifications', notificationsRouter);
 
 module.exports = app;
+
+app.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        auth.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({success: false, message: 'Failed to authenticate token.'});
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
